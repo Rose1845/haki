8a78f092fa287195f6e00480e932f8279a788ddc
Rose1845 <odhiamborose466@gmail.com>
2021-06-21 20:40:44 +0300

    second commit

---
+++ haki.js
@@ -0,0 +1,14 @@
+
+    
+    var maked = new maked("#make", {
+        strings: ["We Make The Law Known"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Knownafrique Legal Services"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
